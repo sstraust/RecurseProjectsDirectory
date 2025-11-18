dde39ad27838059153bfb4a80daa87fdeb485f59
elizathescheisse <elizathescheisse@users.noreply.github.com>
2025-11-18 16:43:19 -0500

    Delete generated files

--- rcprojectsdirjs/out/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { rcprojectsdir.core.load_page(); } catch (e) { console.error("An error occurred when calling (rcprojectsdir.core/load-page)"); throw(e); }
