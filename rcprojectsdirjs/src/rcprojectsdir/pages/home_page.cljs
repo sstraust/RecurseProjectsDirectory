64283559ac4911b662afffef27e425f356e59858
elizathescheisse <elizathescheisse@users.noreply.github.com>
2025-11-21 15:46:11 -0500

    Refactoring to remove class

--- rcprojectsdirjs/src/rcprojectsdir/pages/home_page.cljs
+++ rcprojectsdirjs/src/rcprojectsdir/pages/home_page.cljs
@@ -24,78 +24,83 @@
 (defn default-project-name []
   (str "NewProject_" (compact-date)))
 
+(defn get-users-projects [projects*]
+  (go
+      (let [resp   (<! (http/get "/getUsersProjects"))
+            body   (:body resp)
+            parsed (cond
+                    (map? body) body
+                    (string? body)
+                    (js->clj (js/JSON.parse body) :keywordize-keys true)
+                    :else {})]
+          ;;  body   (:body resp)]
+        (when-let [projects (:projects parsed)]
+          (reset! projects* projects))))
+)
+
+(defn create-project-fn [project-name* desc*]
+  (fn [e]
+        (.preventDefault e)
+        (when-not (seq @desc*)
+          (js/alert "Please enter a description for your project"))
+        (when (seq @desc*)
+          (go
+            (let [result (<! (http/post "/newProject"
+                                        {:form-params {:project-name        @project-name*
+                                                      :project-description @desc*}}))
+                status (:status result)
+                body   (some-> (:body result) (js/JSON.parse) (js->clj :keywordize-keys true))]
+              (if (= status 200)
+                (do 
+                  (reset! desc* "")
+                  (js/alert "Project created!"))
+                (do
+                  (.error js/console "Failed to create project" (clj->js result))
+                  (js/alert (or (:error body) "Something went wrong creating your project."))))))))
+)
+
 (defn update-project []
   (let [desc*         (r/atom "")
         project-name* (r/atom (default-project-name))
         projects*     (r/atom [])]
 
-    (r/create-class
-      {:component-did-mount
-       (fn []
-         (go
-           (let [resp   (<! (http/get "/getUsersProjects"))
-                 body   (:body resp)
-                 parsed (cond
-                          (map? body) body
-                          (string? body)
-                          (js->clj (js/JSON.parse body) :keywordize-keys true)
-                          :else {})]
-                ;;  body   (:body resp)]
-             (when-let [projects (:projects parsed)]
-               (reset! projects* projects)))))
+    (get-users-projects projects*)
 
-       :reagent-render
-       (fn []
-         [:form.w-full.px-16
-          {:on-submit
-           (fn [e]
-             (.preventDefault e)
-             (when-not (seq @desc*)
-               (js/alert "Please enter a description for your project"))
-             (when (seq @desc*)
-               (go
-                 (let [result (<! (http/post "/newProject"
-                                             {:form-params {:project-name        @project-name*
-                                                            :project-description @desc*}}))
-                      status (:status result)
-                      body   (some-> (:body result) (js/JSON.parse) (js->clj :keywordize-keys true))]
-                    (if (= status 200)
-                      (do 
-                        (reset! desc* "")
-                        (js/alert "Project created!"))
-                      (do
-                        (.error js/console "Failed to create project" (clj->js result))
-                        (js/alert (or (:error body) "Something went wrong creating your project."))))))))}
+  (fn []
+    [:form.w-full.px-16
+    {:on-submit
+      (create-project-fn project-name* desc*)
+    }
 
-          ;; Description input 
-          [:div
-           [:input.input.input-bordered.w-full
-            {:type        "text"
-             :placeholder "Tell us about your project"
-             :value       @desc*
-             :on-change   #(reset! desc* (.. % -target -value))}]]
+    ;; Description input 
+    [:div
+      [:input.input.input-bordered.w-full
+      {:type        "text"
+        :placeholder "Tell us about your project"
+        :value       @desc*
+        :on-change   #(reset! desc* (.. % -target -value))}]]
 
-          ;; Row with dropdown and button
-          [:div.flex.items-center.justify-between.my-4
-           ;; left side: dropdown 
-           [:div.self-start
-            [:select
-             {:class "select select-bordered select-xs opacity-70 subtle-select"
-             :value @project-name*
-              :on-change #(reset! project-name* (.. % -target -value))}
-             ;; Default option
-             [:option {:value (default-project-name)}
-              (default-project-name)]
-             ;; Existing projects
-             (for [{:keys [id name]} @projects*]
-               ^{:key id}
-               [:option {:value name} name])]]
+    ;; Row with dropdown and button
+    [:div.flex.items-center.justify-between.my-4
+      ;; left side: dropdown 
+      [:div.self-start
+      [:select
+        {:class "select select-bordered select-xs opacity-70 subtle-select"
+        :value @project-name*
+        :on-change #(reset! project-name* (.. % -target -value))}
+        ;; Default option
+        [:option {:value (default-project-name)}
+        (default-project-name)]
+        ;; Existing projects
+        (for [{:keys [id name]} @projects*]
+          ^{:key id}
+          [:option {:value name} name])]]
 
-           ;; right side: button
-           [:div
-            [:button.btn.btn-primary
-             {:type "submit"}
-             "Create"]]]])})))
+      ;; right side: button
+      [:div
+      [:button.btn.btn-primary
+        {:type "submit"}
+        "Create"]]]])))
 
 (defn featured []
   [:div.h-64.w-full.bg-base-300.my-2
