0762d77b0b8e5c98f989ac4d044afd2bca0579ba
sam <sammy>
2025-11-24 14:50:44 -0500

    add more stuff

--- src/rcprojectsdir/run_web_server.clj
+++ src/rcprojectsdir/run_web_server.clj
@@ -19,8 +19,6 @@
 (def recurse-token-url "https://www.recurse.com/oauth/token")
 (def recurse-handle-auth-redirect-url "http://localhost:8001/handleRedirectResponse")
 
-(def fake-user-id 2)
-
 
 (def db-spec
   {:dbtype "postgresql"
