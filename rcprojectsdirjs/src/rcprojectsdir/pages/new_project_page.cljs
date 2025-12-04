db756360c5397ca2b4c1dbe1cb2e33f026957eae
sam <sammy>
2025-12-04 13:13:30 -0500

    restyle initial landing page

--- rcprojectsdirjs/src/rcprojectsdir/pages/new_project_page.cljs
+++ rcprojectsdirjs/src/rcprojectsdir/pages/new_project_page.cljs
@@ -15,15 +15,120 @@
         (js/alert "Failed to Submit")))))
 
 
-(defn create-project-field [content* label-name placeholder]
+(defn create-project-field [content* label-name placeholder subtext]
   [:<>
-    [:label.font-semibold.my-4
+   [:label.font-semibold
+    {:style {:font-size "1.25rem"
+             :height "3.3125rem"
+             :margin-bottom "0.625rem"}}
      label-name]
     [:input.w-full.input.input-bordered
      {:type "text"
+      :style {:font-size "1.25rem"}
       :value       @content*
       :on-change   #(reset! content* (.. % -target -value))
-      :placeholder placeholder}]])
+      :placeholder placeholder}]
+   [:p.font-medium
+    {:style
+     {:height "0.875rem"
+      :color "#717171"
+      :font-size "0.75rem"
+      :margin-top "0.625rem"}}
+    subtext]
+   ])
+
+
+(defn create-project-field-description [content* label-name placeholder subtext]
+  [:<>
+   [:label.font-semibold
+    {:style {:font-size "1.25rem"
+             :margin-bottom "0.625rem"}}
+    label-name]
+   [:textarea.w-full.textarea.textarea-bordered
+    {:style {:width "41.0625rem"
+             :height "14.5625rem"
+             :font-size "1.25rem"
+             :font-weight 400
+             :max-width "none"
+             :max-height "none"}
+     :value       @content*
+     :on-change   #(reset! content* (.. % -target -value))
+     :placeholder placeholder}]
+   [:p.font-medium
+    {:style
+     {:height "0.875rem"
+      :color "#717171"
+      :font-size "0.75rem"
+      :margin-top "0.625rem"}}
+    subtext]
+   ])
+
+(defn create-project-field-link [content* label-name placeholder subtext]
+  [:<>
+   [:label.font-semibold
+    {:style {:font-size "1.25rem"
+             :margin-bottom "0.625rem"}}
+    label-name]
+   [:h-box.items-center
+    [:input.input.input-bordered.flex-grow
+     {:type "text"
+      :style {:font-size "1.25rem"
+              :height "3.3125rem"}
+      :value       @content*
+      :on-change   #(reset! content* (.. % -target -value))
+      :placeholder placeholder}]
+    [:h-box.items-center
+     {:style {:min-width "6.9375rem"
+              :font-size "1.25rem"
+              :margin-left "1.875rem"}}
+     [:label {:style {:white-space "nowrap"
+                      :color "#353535"}}
+      "Is it Live?"]
+     [:input.checkbox {:type "checkbox"
+                       :style {:margin-left "0.5625rem"}
+                       }]]]
+   
+   [:p.font-medium
+    {:style
+     {:height "0.875rem"
+      :color "#717171"
+      :font-size "0.75rem"
+      :margin-top "0.625rem"}}
+    subtext]
+   ])
+
+(defn create-project-screenshots-mockup [content* label-name placeholder subtext]
+  [:<>
+   [:label.font-semibold
+    {:style {:font-size "1.25rem"
+             :margin-bottom "0.625rem"}}
+    label-name]
+   [:h-box.items-center
+    [:input.input.input-bordered.flex-grow
+     {:type "text"
+      :style {:font-size "1.25rem"
+              :height "3.3125rem"
+              }
+      :value       @content*
+      :on-change   #(reset! content* (.. % -target -value))
+      :placeholder placeholder}]
+    [:button.normal-case.font-bold.border.border-1.rounded-xl
+     {:style {:margin-left  "1.875rem"
+              :min-width "9.125rem"
+              :min-height "3.3125rem"
+              :font-size "1.5rem"
+              :line-height "100%"}}
+     "Browse"]]
+   
+   [:p.font-medium
+    {:style
+     {:height "0.875rem"
+      :color "#717171"
+      :font-size "0.75rem"
+      :margin-top "0.625rem"
+      }}
+    subtext]
+   ])
 
 
 (defn create-project-fn [project-details-atom]
@@ -53,49 +158,72 @@
 (defn create-your-first-project [{:keys [title-text]}]
   (let [project-details-atom (r/atom {})]
     (fn []
-  [:div.w-screen.h-screen.bg-base-200
+  [:div.w-screen.h-screen
    [:div.hero
    [:div.heroed-content.mt-10
-  [:v-box.items-center.card.shadow-lg.bg-base-100.pb-10
+  [:v-box.items-center.bg-base-100.pb-10
    {:style
     {:font-family "Raleway"
-     :width "39.375rem"
+     :min-width "41.0625rem"
+     :width "41.0625rem"
      ;; :height "44.5rem"
      }}
-   [:h1.font-bold.m-10 {:style {:font-size "2.185rem"}}
+   [:h1.font-bold.mt-10.w-full
+    {:style {:font-size "1.875rem"
+             :height "2.1875rem"
+             :line-height "100%"}}
     title-text]
+   [:p.w-full
+    {:style {:font-size "1.25rem"}}
+    "If not, you can skip to go straight into the app. You can always add a project later."]
 
    [:v-box.items-center.w-full
     [:v-box.self-stretch
-     {:style {:padding-left "2.5rem"
-              :padding-right "2.5rem"}}
+     ;; {:style {:padding-left "2.5rem"
+     ;;          :padding-right "2.5rem"}}
 
-   [create-project-field (r/cursor project-details-atom [:name]) "Name" ;; "NewProject_11-25-25"
-    "MyCoolProject"]
-   [:div {:style {:height 28}}  " "]
-   [create-project-field (r/cursor project-details-atom [:description]) "Description" "Catch up on your friends projects"]
-   [:div {:style {:height 28}}  " "]
-   [create-project-field (r/cursor project-details-atom [:link]) "Link" "github.com/name/thisproject"]
+     [:div {:style {:height "2.5rem"}} " "]
+     [create-project-field (r/cursor project-details-atom [:name]) "Name" ;; "NewProject_11-25-25"
+      "MyCoolProject"
+      "(Defaults to today’s date- you can change this later)"
+      ]
+     [:div {:style {:height "1.875rem"}}  " "]
+     [create-project-field-description (r/cursor project-details-atom [:description]) "Description" "Starting to work on a new app..."
+      "(Can just be one line)"]
+     [:div {:style {:height "1.875rem"}}  " "]
+     [create-project-field-link (r/cursor project-details-atom [:link]) "Link" "github.com/name/thisproject"
+      "(Optional)"]
+     [:div {:style {:height "1.875rem"}}  " "]
+     [create-project-screenshots-mockup (r/cursor project-details-atom [:link]) "Screenshots" "Drag and drop or \"Browse\" to upload"
+      "(Optional)"]
      
      [:div {:style {:height 28}}  " "]     
-     [:div.w-full
-      [:div "+ Add Additional Info"]]
+     ;; [:div.w-full
+     ;;  [:div "+ Add Additional Info"]]
 
      [:h-box.items-center.self-end.gap-3
 
       ;; now you just need to add these routes
       ;; which set the is new user value
-      [:button.btn.btn-outline
-       {:on-click skip-submitted}
+      [:button.btn.btn-outline.normal-case.rounded-xl
+       {:on-click skip-submitted
+        :style {:width "6.875rem"
+                :max-width "none"
+                :font-size "1.5rem"
+                :height "4.25rem"}}
        "Skip"]
-      [:button.btn.btn-primary
-       {:on-click (create-project-fn project-details-atom)}
+      [:button.btn.btn-primary.normal-case.rounded-xl
+       {:on-click (create-project-fn project-details-atom)
+        :style {:width "8.625rem"
+                :max-width "none"
+                :font-size "1.5rem"
+                :height "4.25rem"}}
        "Create"]]]]]]]])))
 
 
 
 (defn load-new-project-view-first-user []
-  (rdom/render [create-your-first-project {:title-text "Create Your First Project"}]
+  (rdom/render [create-your-first-project {:title-text "Do you have a project you'd like to add?"}]
                (js/document.getElementById "main-app")))
 
 (defn load-new-project-view []
